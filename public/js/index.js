d5330b23b1c8401540baa19d00adf47c652a716d
vincet <g25259@live.com>
2017-12-16 14:38:50 +0800

    Interactive  among users

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-    socket.emit('createMessage', {
-        from: 'Jean',
-        text: 'test',
-    });
 });
 
 socket.on('disconnect', function() {
@@ -14,4 +10,20 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
    console.log(message);
+   var li = jQuery('<li></li>');
+   li.text(`${message.from}: ${message.text}`);
+
+   jQuery('#msgs').append(li);
+});
+
+
+jQuery('#msg-form').on('submit', function (e) {
+    console.log(e);
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=msg]').val()
+    }, function () {
+        
+    });
 });
